95bf03fe086d5f4f9297b5b2a0a6fdb0e232cc79
Alexandra Irwin <airwin357@gmail.com>
2024-06-10 11:58:50 +1000

    1158am commit

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import sty from './track.module.css';
 
 export default function Track(props) {
     return (
-        <div>
+        <div className={sty}>
             <p>{props.name}</p>
             <p>{props.artist}</p>
             <p>{props.album}</p>
             <p>{props.id}</p>
+            <p>&#43; &#x2B; &plus;</p>
+           
         </div>
     )
 }
